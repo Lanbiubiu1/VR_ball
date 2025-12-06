e21c56559b40d7d737f5a507d8a5d25b384e2cb2
Sirui Zhu <siruizhu@usc.edu>
2025-12-06 05:11:46 -0500

    Added Start and two end screens

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -106,5 +107,15 @@ public class GameManager : MonoBehaviour
         // update UI with new level’s totals
         ScoreUI.Instance.UpdateText(hitCount, totalGhosts);
         ScoreUI.Instance.UpdateLevelText(currentLevel);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
