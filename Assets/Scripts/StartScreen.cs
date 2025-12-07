c00b5ed2aedc69a52ca54af54878f0ffb009c9c6
Crystal Jin <kstjxy@gmail.com>
2025-12-07 00:33:50 -0500

    add button interaction

--- Assets/Scripts/StartScreen.cs
+++ Assets/Scripts/StartScreen.cs
@@ -1,10 +1,69 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR;
 
 public class StartScreen : MonoBehaviour
 {
+    private InputDevice leftHand;
+    private InputDevice rightHand;
+
+    private bool lastState = false;
+
+    void Start()
+    {
+        GetDevices();
+    }
+
+    void GetDevices()
+    {
+        var devices = new List<InputDevice>();
+
+        // Left controller
+        InputDevices.GetDevicesAtXRNode(XRNode.LeftHand, devices);
+        if (devices.Count > 0)
+            leftHand = devices[0];
+
+        // Right controller
+        InputDevices.GetDevicesAtXRNode(XRNode.RightHand, devices);
+        if (devices.Count > 0)
+            rightHand = devices[0];
+    }
+
+    void Update()
+    {
+        // Reacquire if lost
+        if (!leftHand.isValid || !rightHand.isValid)
+        {
+            GetDevices();
+            return;
+        }
+
+        // 🔥 Check ANY button on EITHER controller
+        bool pressed =
+            GetPress(leftHand) ||
+            GetPress(rightHand);
+
+        // Trigger once on press-down (not hold)
+        if (pressed && !lastState)
+        {
+            StartGame();
+        }
+
+        lastState = pressed;
+    }
+
+    bool GetPress(InputDevice device)
+    {
+        // Check a list of common buttons
+        return
+            (device.TryGetFeatureValue(CommonUsages.primaryButton, out bool primary) && primary) ||     // A / X
+            (device.TryGetFeatureValue(CommonUsages.secondaryButton, out bool secondary) && secondary) || // B / Y
+            (device.TryGetFeatureValue(CommonUsages.triggerButton, out bool trigger) && trigger) ||
+            (device.TryGetFeatureValue(CommonUsages.gripButton, out bool grip) && grip) ||
+            (device.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out bool stickClick) && stickClick);
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("Main");
